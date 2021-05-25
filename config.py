f48aa20c3d282cb46f0ab3597f057892b29d3e38
sohailkhan-project <78003723+sohailkhan-project@users.noreply.github.com>
2021-05-25 07:13:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+TG_BOT_TOKEN = "1837046441:AAH74-APff1xZN1vgyHh7SWfP-GBDcfKIyk"
+GDRIVE_FOLDER_ID = "1pY7ZcjY2yXPjajZr_bFVSAxGC31huOHf"
+APP_ID = 1089218
+API_HASH = "b6bdbb931516e5f9405f96ae129a412a"
+OWNER_ID = 1230663194
+AUTH_CHANNEL = [l-1001206840005]
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+TRACKERSLIST = "https://trackerslist.com/all_aria2.txt"
+USE_TELEGRAPH = "true"
+STOP_DUPLICATE_MIRROR = "true"
+INDEX_URL = "https://files.indianime.workers.dev"
+IS_TEAM_DRIVE = true
+RCLONE_CONFIG  = '''type = drive
+scope = drive
+token = {"access_token":"ya29.a0AfH6SMCg5QcjHCaScmi_W5ofynucQt7udIPL3xvmcoDyZaFXaa3eZAz1tVdKYdP9Ieu-mP973l0OsUiDVuNO7e84Ikr_lylqnZb3nUC510oSGVzuesKa4kI10r4U80D285fbUr62X_cRVS7ppe1uY_lNU_KH","token_type":"Bearer","refresh_token":"1//0gK9ZjDCSIwjLCgYIARAAGBASNwF-L9IrRmg-UZoMzEC31VV0_xaR1SuzgxnJ3Ixk9n4OZq4wyx0fYkCj7te1OrmP7tjm-lfhHGg","expiry":"2021-05-25T02:06:57.503233771Z"}
+client_id = 186130026096-08igmpiaeriudosof24h4io9nbv9h6qd.apps.googleusercontent.com
+client_secret = 0V8A-mHTBlXSQ_AODM0BoKNI '''
